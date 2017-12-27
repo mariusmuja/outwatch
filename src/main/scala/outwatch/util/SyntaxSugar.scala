e3297292dff9729bf0372b08bc111a9c014559cb
Marius Muja <mariusmuja@gmail.com>
2017-12-27 00:34:42 -0800

    Conditional VDomModifier syntax sugar

--- src/main/scala/outwatch/util/SyntaxSugar.scala
+++ src/main/scala/outwatch/util/SyntaxSugar.scala
@@ -1,7 +1,7 @@
 package outwatch.util
 
 import cats.effect.IO
-import outwatch.dom.{Attribute, AttributeStreamReceiver, Observable, TitledAttribute}
+import outwatch.dom.{Attribute, AttributeStreamReceiver, Observable, TitledAttribute, VDomModifier}
 
 
 object SyntaxSugar {
@@ -12,6 +12,20 @@ object SyntaxSugar {
         val attributes = values.map(b => if (b) attr else Attribute.empty)
         AttributeStreamReceiver(attr.title, attributes)
       }
+    }
+  }
+
+
+  implicit class BooleanVDomSelector(val condition: Boolean) extends AnyVal {
+    def ?=[T](vDomModifier: => T)(implicit conv: T => VDomModifier): VDomModifier = {
+      if (condition) conv(vDomModifier) else VDomModifier.empty
+    }
+  }
+
+
+  implicit class OptionalVDomModifier[T](vDomModifier: => T)(implicit conv: T => VDomModifier) {
+    def when(condition: Boolean): VDomModifier = {
+      if (condition) conv(vDomModifier) else VDomModifier.empty
     }
   }
 
