a79ec12cdc507195f325a93a2bd77facf3386404
Marius Muja <mariusmuja@gmail.com>
2018-01-16 00:30:06 -0800

    TargetOps merge from upstream

--- src/main/scala/outwatch/dom/helpers/TargetOps.scala
+++ src/main/scala/outwatch/dom/helpers/TargetOps.scala
@@ -1,27 +1,25 @@
 package outwatch.dom.helpers
 
-import org.scalajs.dom.{Event, EventTarget, html}
+import org.scalajs.dom.{Event, html}
+import outwatch.dom.Emitter
 
 trait TargetOps {
 
-  implicit class EventTargetAsInput[E <: Event, ET <: EventTarget](builder: EmitterBuilder[E, ET]) {
-    def value: EmitterBuilder[E, String] = builder.map(_.asInstanceOf[html.Input].value)
+  implicit class TargetAsInput[E <: Event, O <: Event](builder: EmitterBuilder[E, O, Emitter]) {
 
-    def valueAsNumber: EmitterBuilder[E, Int] = builder.map(_.asInstanceOf[html.Input].valueAsNumber)
+    object target {
+      def value: EmitterBuilder[E, String, Emitter] = builder.map(_.target.asInstanceOf[html.Input].value)
 
-    def checked: EmitterBuilder[E, Boolean] = builder.map(_.asInstanceOf[html.Input].checked)
-  }
+      def valueAsNumber: EmitterBuilder[E, Int, Emitter] = builder.map(_.target.asInstanceOf[html.Input].valueAsNumber)
 
-  implicit class CurrentTargetAsInput[E <: Event, O <: Event](builder: EmitterBuilder[E, O]) {
-    def value: EmitterBuilder[E, String] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].value)
+      def checked: EmitterBuilder[E, Boolean, Emitter] = builder.map(_.target.asInstanceOf[html.Input].checked)
+    }
 
-    def valueAsNumber: EmitterBuilder[E, Int] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].valueAsNumber)
+    def value: EmitterBuilder[E, String, Emitter] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].value)
 
-    def checked: EmitterBuilder[E, Boolean] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].checked)
-  }
+    def valueAsNumber: EmitterBuilder[E, Int, Emitter] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].valueAsNumber)
 
-  implicit class TargetMap[E <: Event, O <: Event](builder: EmitterBuilder[E, O]) {
-    def target: EmitterBuilder[E, EventTarget] = builder.map(_.target)
+    def checked: EmitterBuilder[E, Boolean, Emitter] = builder.map(e => e.currentTarget.asInstanceOf[html.Input].checked)
   }
 
 }
