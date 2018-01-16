0af36687120b2ae1f955a9a38ffa00db4b59c777
Marius Muja <mariusmuja@gmail.com>
2018-01-16 00:30:34 -0800

    StorageHandler

--- src/main/scala/outwatch/util/LocalStorage.scala
+++ src/main/scala/outwatch/util/LocalStorage.scala
@@ -2,20 +2,38 @@ package outwatch.util
 
 import cats.effect.IO
 import monix.execution.Ack.Continue
-import monix.execution.{Cancelable, Scheduler}
-import monix.reactive.OverflowStrategy.Unbounded
-import outwatch.Sink
+import monix.execution.Scheduler
+import org.scalajs.dom.Storage
 import org.scalajs.dom.window.localStorage
 import outwatch.dom.Observable
+import outwatch.dom.dsl.events.window.onStorage
+import outwatch.{Handler, Sink}
 
 
-object LocalStorageReader {
-  def apply(key: String): Observable[String] = {
-    Observable.create[String](Unbounded){ observer =>
-      observer.onNext(localStorage.getItem(key))
-      Cancelable.empty
+class StorageHandler(storageArea: Storage)(key: String)(implicit s: Scheduler) {
+
+  def value: Observable[String] = Observable(storageArea.getItem(key))
+
+  val handler: IO[Handler[Option[String]]] = Handler.create[Option[String]].map { handler =>
+
+    val otherPageChange = onStorage.filter(x => x.storageArea == storageArea && x.key == key)
+      .map(e => Option(e.newValue).map(_.asInstanceOf[String]))
+
+    handler.transformSource { orig =>
+      val obs = orig.map { value =>
+        value.fold(storageArea.removeItem(key))(value =>
+          storageArea.setItem(key, value)
+        )
+        value
+      }.startWith(Seq(Option(storageArea.getItem(key))))
+
+      Observable.merge(obs, otherPageChange).share
     }
   }
+}
+
+object LocalStorageReader {
+  def apply(key: String): Observable[String] = Observable(localStorage.getItem(key))
 }
 
 object LocalStorageWriter {
