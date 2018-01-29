9fb2ec7578af806a1502a737dbc13cfead2db272
Marius Muja <mariusmuja@gmail.com>
2018-01-29 00:25:55 -0800

    Use AsDomModifier in SyntaxSuger

--- src/main/scala/outwatch/util/SyntaxSugar.scala
+++ src/main/scala/outwatch/util/SyntaxSugar.scala
@@ -1,31 +1,32 @@
 package outwatch.util
 
 import cats.effect.IO
+import outwatch.AsVDomModifier
 import outwatch.dom.{Attribute, AttributeStreamReceiver, Observable, TitledAttribute, VDomModifier}
 
 
 object SyntaxSugar {
 
-  implicit class BooleanSelector(val values: Observable[Boolean]) extends AnyVal {
+  implicit class BooleanSelector(val conditionStream: Observable[Boolean]) extends AnyVal {
     def ?=(attr: IO[TitledAttribute]): IO[AttributeStreamReceiver] = {
       attr.map { attr =>
-        val attributes = values.map(b => if (b) attr else Attribute.empty)
-        AttributeStreamReceiver(attr.title, attributes)
+        val attributeStream = conditionStream.map(condition => if (condition) attr else Attribute.empty)
+        AttributeStreamReceiver(attr.title, attributeStream)
       }
     }
   }
 
 
   implicit class BooleanVDomSelector(val condition: Boolean) extends AnyVal {
-    def ?=[T](vDomModifier: => T)(implicit conv: T => VDomModifier): VDomModifier = {
-      if (condition) conv(vDomModifier) else VDomModifier.empty
+    def ?=[T : AsVDomModifier](vDomModifier: => T): VDomModifier = {
+      if (condition) vDomModifier else VDomModifier.empty
     }
   }
 
 
-  implicit class OptionalVDomModifier[T](vDomModifier: => T)(implicit conv: T => VDomModifier) {
+  implicit class OptionalVDomModifier[T : AsVDomModifier](vDomModifier: => T) {
     def when(condition: Boolean): VDomModifier = {
-      if (condition) conv(vDomModifier) else VDomModifier.empty
+      if (condition) vDomModifier else VDomModifier.empty
     }
   }
 
