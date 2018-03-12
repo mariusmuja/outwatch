a4c893836434b0eed272b6db5cef42b6247ded19
Marius Muja <marius.muja@bitlit.ca>
2018-03-12 14:06:18 -0700

    Store updates

--- src/test/scala/outwatch/ScenarioTestSpec.scala
+++ src/test/scala/outwatch/ScenarioTestSpec.scala
@@ -4,6 +4,7 @@ import org.scalajs.dom.{html, _}
 import outwatch.Deprecated.IgnoreWarnings.initEvent
 import outwatch.dom._
 import outwatch.dom.dsl._
+import outwatch.util.Store
 
 object ScenarioTestSpec extends JSDomSuite {
 
@@ -46,6 +47,52 @@ object ScenarioTestSpec extends JSDomSuite {
       document.getElementById("counter").innerHTML shouldBe i.toString
     }
     ()
+  }
+
+
+  test("A simple counter application that uses Store should work as intended") {
+
+    sealed trait Action
+    case object Plus extends Action
+    case object Minus extends Action
+
+    type State = Int
+
+    def reduce(state: State, action: Action): State = action match {
+      case Plus => state + 1
+      case Minus => state - 1
+    }
+
+    val node = for {
+      store <- Store.create[State, Action](0, reduce _)
+
+      div <- div(
+        div(
+          button(id := "plus", "+", onClick(Plus) --> store),
+          button(id := "minus", "-", onClick(Minus) --> store),
+          span(id:="counter", store)
+        )
+      )
+    } yield div
+
+    val root = document.createElement("div")
+    document.body.appendChild(root)
+
+    OutWatch.renderInto(root, node).unsafeRunSync()
+
+    val event = document.createEvent("Events")
+    initEvent(event)("click", canBubbleArg = true, cancelableArg = false)
+
+    document.getElementById("counter").innerHTML shouldBe 0.toString
+
+    document.getElementById("minus").dispatchEvent(event)
+
+    document.getElementById("counter").innerHTML shouldBe (-1).toString
+
+    for (i <- 0 to 10) {
+      document.getElementById("plus").dispatchEvent(event)
+      document.getElementById("counter").innerHTML shouldBe i.toString
+    }
   }
 
   test("A simple name application should work as intended") {
