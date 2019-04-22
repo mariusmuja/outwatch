890a3823dd9bd8116d38517f05fc00f4fb9ed5d6
Marius Muja <mariusmuja@gmail.com>
2019-04-21 22:24:20 -0700

    SyntexSugar fixes/updates

--- src/main/scala/outwatch/util/SyntaxSugar.scala
+++ src/main/scala/outwatch/util/SyntaxSugar.scala
@@ -1,16 +1,17 @@
 package outwatch.util
 
+import outwatch.AsVDomModifier
 import outwatch.dom.{Observable, VDomModifier}
 
 private[util] final class BooleanSelector(private val conditionStream: Observable[Boolean]) extends AnyVal {
-  def ?=(mod: VDomModifier): VDomModifier = {
-    conditionStream.map(cond => if (cond) mod else VDomModifier.empty)
+  def ?=[T: AsVDomModifier](mod: => T): VDomModifier = {
+    conditionStream.map(cond => if (cond) AsVDomModifier[T].asVDomModifier(mod) else VDomModifier.empty)
   }
 }
 
 private[util] final class BooleanVDomSelector(private val condition: Boolean) extends AnyVal {
-  def ?=[T](vDomModifier: => T)(implicit conv: T => VDomModifier): VDomModifier = {
-    if (condition) conv(vDomModifier) else VDomModifier.empty
+  def ?=[T: AsVDomModifier](vDomModifier: => T): VDomModifier = {
+    if (condition) AsVDomModifier[T].asVDomModifier(vDomModifier) else VDomModifier.empty
   }
 }
 
